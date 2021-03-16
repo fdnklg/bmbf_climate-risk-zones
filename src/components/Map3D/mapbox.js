e9ff88eaaaa7aada82d74217a70f40f72032e051
Sebastian Meier <contact@sebastianmeier.eu>
2021-03-16 21:48:41 +0100

    move mapbox key to env

--- src/components/Map3D/mapbox.js
+++ src/components/Map3D/mapbox.js
@@ -1,7 +1,6 @@
 import mapbox from 'mapbox-gl'
 
-mapbox.accessToken =
-  'pk.eyJ1IjoianVsaTg0IiwiYSI6ImNrbHRpOGpxbzBrM3Qyd254dnM5eHd3NDYifQ.8nh2gkRqnS-9vyo4arAYSA'
+mapbox.accessToken = __env__.MAPBOXKEY
 
 const key = {}
 
